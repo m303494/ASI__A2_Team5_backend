8698e34c22c5ccb9cc79dc6dc1b75100e66c3602
m303494 <m303494@alunos.ipb.pt>
2020-06-18 20:01:19 +0200

    FileUpload FIXED

---
+++ ASI__A2_Team5-master/A2UserCRUD/Controller/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+namespace A2UserCRUD.Controller
+{
+    [Produces("application/json")]
+    [Route("api/Image")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IHostingEnvironment _environment;
+        public ImageController(IHostingEnvironment environment)
+        {
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
+        // POST: api/Image
+        [HttpPost]
+        public async Task Post(IFormFile file)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            if (file.Length > 0)
+            {
+                using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+        }
+    }
+}
